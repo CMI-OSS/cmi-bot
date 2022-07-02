562b105c29f122ec1f6b303e6fcac342fdd4e581
제스 <jaryapp@gmail.com>
2022-07-02 13:50:10 +0900

    ✨ modify 프론트엔드 회의시간

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 
 const indexRouter = require("./routes/index");
 const scheduleGithub = require("./app/github");
-const {sendConferenceRemind, sendStudyRemind} = require("./app/slack")
+const { sendConferenceRemind, sendStudyRemind } = require("./app/slack");
 
 const app = express();
 
@@ -18,9 +18,9 @@ app.use(cookieParser());
 
 app.use("/", indexRouter);
 
-const 충림이_백엔드 = "C02NXF9QKRS"
-const 충림이_프론트엔드 = "C02NEHY2CNB"
-const 타입스크립트_스터디 = "C036PG3K2JY"
+const 충림이_백엔드 = "C02NXF9QKRS";
+const 충림이_프론트엔드 = "C02NEHY2CNB";
+const 타입스크립트_스터디 = "C036PG3K2JY";
 
 const dontForget = schedule.scheduleJob("0 0 16 * * 1-5", function () {
   scheduleGithub();
@@ -28,21 +28,20 @@ const dontForget = schedule.scheduleJob("0 0 16 * * 1-5", function () {
 
 //  const WEEKDAY = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
 
-
-
-
-schedule.scheduleJob({hour: 10, minute: 50, dayOfWeek: 0}, function(){
-  sendStudyRemind({study:"타입스크립트 스터디",channelId:타입스크립트_스터디,link:"https://github.com/CMI-OSS/study/tree/main/typescript"})
+schedule.scheduleJob({ hour: 10, minute: 50, dayOfWeek: 0 }, function () {
+  sendStudyRemind({
+    study: "타입스크립트 스터디",
+    channelId: 타입스크립트_스터디,
+    link: "https://github.com/CMI-OSS/study/tree/main/typescript",
+  });
 });
 
- schedule.scheduleJob({hour: 21, minute: 50, dayOfWeek: 6}, function(){
-  sendConferenceRemind({team:"프론트엔드",channelId:충림이_프론트엔드})
+schedule.scheduleJob({ hour: 10, minute: 20, dayOfWeek: 0 }, function () {
+  sendConferenceRemind({ team: "프론트엔드", channelId: 충림이_프론트엔드 });
 });
 
- schedule.scheduleJob({hour: 9, minute: 50, dayOfWeek: 0}, function(){
-  sendConferenceRemind({team:"백엔드",channelId:충림이_백엔드})
+schedule.scheduleJob({ hour: 9, minute: 50, dayOfWeek: 0 }, function () {
+  sendConferenceRemind({ team: "백엔드", channelId: 충림이_백엔드 });
 });
-
-
 
 module.exports = app;
